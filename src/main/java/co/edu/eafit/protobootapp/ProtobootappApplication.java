0386c9744d386024ecf53062854d0a76c2c46a05
Carla Daniela Rendon <carla.rendon@talosdigital.com>
2021-06-09 14:39:12 -0500

    commit in test branch

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Esta aplicación ayuda a elevar potencias. c: this is  a test");
+        return String.format("Bienvenido! Esta aplicación ayuda a elevar potencias. c: this is  a test, from the test branch");
     }    
 
 //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
